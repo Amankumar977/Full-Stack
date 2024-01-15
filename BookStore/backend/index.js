4464e1eb5576b1c553faa1f5aae4375de850dbaa
Aman Kumar <ak9330674@gmail.com>
2024-01-15 21:31:02 +0530

    Done

--- BookStore/backend/index.js
+++ BookStore/backend/index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://book-management-front.vercel.app/",
+    origin: "https://book-management-front.vercel.app",
   })
 );
 
